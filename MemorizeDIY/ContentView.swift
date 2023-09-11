c3f780548504b244f0a356f89d823cfff4dd18fb
Gregory <gregorysyrkin@gmail.com>
2023-09-11 22:13:45 +0200

    add theme sets and theme-switching controls; scrollView and LazyVGrid instead of HStack

--- MemorizeDIY/ContentView.swift
+++ MemorizeDIY/ContentView.swift
@@ -8,28 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
-    let deck = ["🏀","🎱","🎾","🏈","🏐","⚽️","⚾️","🎈"]
-    var currentNumberOfCards = 4
+    enum Theme {
+        case WINTER, HALLOWEEN, BALL_SPORTS
+    }
+    let WINTER_THEME_ITEMS = ["🤶","🎅","🛷","🥶","☃️","❄️","🥌","🏂", "🤶","🎅","🛷","🥶","☃️","❄️","🥌","🏂"]
+    let HALLOWEEN_THEME_ITEMS = ["🎃","👻","💀","☎️","🎞️","⏱️","🔌","⚖️", "🎃","👻","💀","☎️","🎞️","⏱️","🔌","⚖️"]
+    let BALL_SPORTS_THEME_ITEMS = ["🏀","🎱","🎾","🏈","🏐","⚽️","⚾️","🎈", "🏀","🎱","🎾","🏈","🏐","⚽️","⚾️","🎈"]
+    @State var currentTheme: Theme = Theme.WINTER
+    
+    var currentThemeItems: [String] {
+        switch currentTheme {
+        case .WINTER:
+            return WINTER_THEME_ITEMS.shuffled()
+        case .HALLOWEEN:
+            return HALLOWEEN_THEME_ITEMS.shuffled()
+        case .BALL_SPORTS:
+            return BALL_SPORTS_THEME_ITEMS.shuffled()
+        }
+    }
+    
     var body: some View {
         VStack {
-            HStack{
-                ForEach(0..<currentNumberOfCards) { index in CardView(isFaceUp: true, face: deck[index])
-                    
-                }
-                
-                CardView(face: deck[0])
-//                CardView()
-//                CardView()
-//                CardView()
+            Text("Memorize!")
+                .font(.title)
+            ScrollView{
+                cards
             }
             Spacer()
-            
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            themeSelectorButtons
         }
         .padding()
+    }
+    
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+            ForEach(0..<currentThemeItems.count, id: \.self) {
+                index in CardView(isFaceUp: true, face: currentThemeItems[index])
+                    .aspectRatio(2/3, contentMode: .fit)
+            }
+        }
+    }
+    
+    var themeSelectorButtons: some View {
+        HStack {
+            themeSelectorButton(themeValue: Theme.WINTER, label: "Winter", lookingLike: "snowflake.circle")
+            themeSelectorButton(themeValue: Theme.HALLOWEEN, label: "Halloween", lookingLike: "fish")
+            themeSelectorButton(themeValue: Theme.BALL_SPORTS, label: "Ball sports", lookingLike: "sportscourt")
+        }
+    }
+    
+    func themeSelectorButton(themeValue: Theme, label: String, lookingLike labelImageKey: String) -> some View {
+        Button(action: {currentTheme = themeValue}) {
+            VStack {
+                Text(label).font(.body)
+                Image(systemName: labelImageKey)
+            }
+        }
     }
 }
 
@@ -44,13 +78,11 @@ struct CardView: View {
                 RoundedRectangle(cornerRadius: 10)
                     .fill(.blue)
                     .frame(height: 200)
-                    
             }
             else {
                 RoundedRectangle(cornerRadius: 10)
                     .stroke(.blue, lineWidth: 5)
                     .frame(height: 200)
-                    
                 Text(face)
                     .font(.title)
                     .foregroundColor(.black)
