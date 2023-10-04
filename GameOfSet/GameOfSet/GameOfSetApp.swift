dfa611f9ff04b069fc943e27128ca3ff0573bca0
Bob <bob@Mac-mini-van-Bob.local>
2023-10-04 17:48:32 +0200

    initial GameOfSet commit

---
+++ GameOfSet/GameOfSet/GameOfSetApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameOfSetApp.swift
+//  GameOfSet
+//
+//  Created by Bob on 04.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct GameOfSetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
